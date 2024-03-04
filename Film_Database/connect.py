20c76641546520a56540202c3fa3db503ce0599e
CorneNapier <cornenapier@gmail.com>
2024-03-04 09:42:59 +0000

    Created database files
    Created connection to existing database file, added crud files and report file. Adding subroutine for creating a record for the table in the database

---
+++ Film_Database/connect.py
@@ -0,0 +1,11 @@
+import sqlite3 as sql #import sqlite3 and assigned it an alias
+
+try:
+    # use connect function to open folder then file
+    with sql.connect("Film_Database/filmflix.db") as dbcon:  #dbCon hold the folder and file path
+        #use to execute sql statement with the execute method
+        dbCursor = dbcon.cursor()
+except sql.OperationalError as e: #raise sql error
+    #handling the error
+    print(f'Connection Failed: {e}')
+    
